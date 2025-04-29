0e1a786fd161143b58509912426dd6fbbc956c1b
skittles1 <tmx883@gmail.com>
2025-04-29 23:36:12 +0800

    Increment client version

--- Meridian59.Ogre.Client/OgreClient.h
+++ Meridian59.Ogre.Client/OgreClient.h
@@ -278,7 +278,7 @@ namespace Meridian59 { namespace Ogre
 
       property unsigned char AppVersionMinor
       { 
-         public: virtual unsigned char get() override { return 31; }
+         public: virtual unsigned char get() override { return 32; }
       };
 
       property ::Ogre::Root* Root 
